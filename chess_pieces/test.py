ea4081d326a3f1ac27f8901921dfec7d77a0a4e8
Nozomu Ai <noah.fournis@gmail.com>
2026-04-02 08:58:14 +0200

    t message for your changes. Lines starting

--- chess_pieces/test.py
+++ chess_pieces/test.py
@@ -1,16 +1,16 @@
 # position.py
 class Position:
  def __init__(self, column, row):
- self.column = column
- self.row = row
- def __str__(self):
- return f"{self.column}{self.row}"
-if __name__ == "__main__":
- # Functional tests of the Position class
- p1 = Position("e", 4)
- print(f"Position created: {p1}") # Expected: e4
+    self.column = column
+    self.row = row
+    def __str__(self):
+        return f"{self.column}{self.row}"
+        if __name__ == "__main__":
+            # Functional tests of the Position class
+            p1 = Position("e", 4)
+            print(f"Position created: {p1}") # Expected: e4
 
- p2 = Position("a", 1)
- print(f"Position created: {p2}") # Expected: a1
+            p2 = Position("a", 1)
+            print(f"Position created: {p2}") # Expected: a1
 
- print("Tests Position OK!")
+            print("Tests Position OK!")

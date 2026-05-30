59d4e8d54d4a9d5fa204ddb839640c3baed2d56a
Nozomu Ai <noah.fournis@gmail.com>
2026-05-30 16:11:19 +0200

    added ai player file

---
+++ ai_player.py
@@ -0,0 +1,4 @@
+from player import player
+
+class AIPlayer(player):
+    def __init__(self)
--- position_class_2.py
+++ position_class_2.py
@@ -1,17 +1,25 @@
 from dataclasses import dataclass
-
-@dataclass(frozen=True)
+# dataclass is a decorator that examines the class to find fields
+@dataclass(frozen=True) # frozen=True means the class' values cant be changed after creation
 class Position:
     column: str  # 'a'..'h'
     row: int     # 1..8
 
-    def post_init(self):
+# @dataclass adds this automatically:
+#    def __init__(self, col, row):
+#       self.column = col
+#       self.row = row
+
+    def __post_init__(self): # special dataclass method that automatically runs after __init__
+        print("post")
         if self.column not in "abcdefgh":
             raise ValueError(f"Invalid column: {self.column}")
         if not (1 <= self.row <= 8):
             raise ValueError(f"Invalid row: {self.row}")
+    # checks that the column and row are in the possible values
 
-    def to_tuple(self):
+# convert a chess position with a letter and number into grid coordinates
+    def to_tuple(self): 
         # 0-based internal grid (row, col)
         c = ord(self.column) - ord('a')
         r = 8 - self.row
@@ -22,5 +30,8 @@ class Position:
         r, c = rc
         return Position(chr(ord('a') + c), 8 - r)
 
-    def str(self):
+    def __str__(self):
         return f"{self.column}{self.row}"
+
+test = Position("js",3)
+print({test})

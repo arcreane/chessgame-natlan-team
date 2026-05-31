e4fc1880636610d7fd16465cc03b3fbd5635be18
kamel2468 <mohamedkeltewacy@gmail.com>
2026-05-31 22:16:06 +0200

    Update isCheckMate.py

--- chess_pieces/isCheckMate.py
+++ chess_pieces/isCheckMate.py
@@ -2,20 +2,16 @@ def isCheckMate(self):
 
     king = None
 
-    # find current player's king
-    for piece in self.board.pieces:
+    for piece in self. board.pieces:
         if isinstance(piece, King) and piece.color == self.currentPlayer.color:
-            king = piece
+            King = piece
             break
 
     if king is None:
         return False
 
-    # check if any enemy piece can attack king
-    for piece in self.board.pieces:
-
+    for piece in self. board.pieces:
         if piece.color != king.color:
-
             if piece.isValidMove(king.position, self.board):
                 return True
 
